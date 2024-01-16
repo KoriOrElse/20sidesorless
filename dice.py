11a9ab95f9349cfaf5bb0301fad87bdccbe8dd4c
KoriOrElse <156597413+KoriOrElse@users.noreply.github.com>
2024-01-16 14:11:57 +0000

    a few changes

--- dice.py
+++ dice.py
@@ -1,16 +1,17 @@
 import random
 
-def askinput():
-    value= (roll_dice(int(input("number of sides: "))))
-    return value
-
-def roll_dice(sides):
+def roll_dice(nsides):
     #sides = int(input("number of sides: "))
-    result= random.randint(1, sides)
-    return result 
+    result= random.randint(1, nsides)
+    return result
+
+def askinput():
+    sides= int(input("number of sides: "))
+    value= roll_dice(sides)
+    return value
+ 
 roll_1= askinput()
 roll_2= askinput()
 print("roll_1: ",roll_1)
 print("roll_2: ",roll_2)
 print("total", roll_1 + roll_2)
-
